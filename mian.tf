92b14b30ba3c4e7a623becd0ae3d63e088d03781
Petros Kourampas <pkourampas@gmail.com>
2025-05-23 15:24:59 +0300

    revise infrastructure tree

--- mian.tf
+++ mian.tf
@@ -1,5 +1,4 @@
 provider "aws" {
-  alias = "main"
   region = var.aws_main_vpc_region
 }
 
@@ -10,21 +9,17 @@ provider "aws" {
 
 # ----- Create Main VPC -----
 module "vpc_main" {
-  source = "../modules/vpc/vpc"
+  source = "./modules/vpc"
   aws_vpc_cidr = var.aws_main_vpc_cidr
   aws_vpc_instance_tenancy = local.vpc_tenancy
   aws_vpc_enable_dns_hostnames = true
   aws_vpc_enable_dns_support = true
   aws_vpc_tag_name = "main vpc"
-
-  providers = {
-    aws = aws.main
-  }
 }
 
 # ----- Create Dr VPC -----
 module "vpc_dr" {
-  source = "../modules/vpc/vpc"
+  source = "./modules/vpc"
   aws_vpc_cidr = var.aws_dr_vpc_cidr
   aws_vpc_instance_tenancy = local.vpc_tenancy
   aws_vpc_enable_dns_hostnames = true
